38e343c6b4a79c90a6f4d742d8fdabed5ab8b8a5
ckylieoroga <christine.kylie@elitesdsi.com>
2025-09-22 15:44:38 +0800

    Added migration drop column image_proof ticket request

---
+++ database/migrations/2025_09_22_074034_drop_column_image_proof_ticket_request.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('ticket_request', function (Blueprint $table) {
+            $table->dropColumn('image_proof');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
